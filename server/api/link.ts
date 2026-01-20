2de1d441a5dc203f070eaa8ae4dca1bd11e447d3
tinarskii <tinvv@outlook.co.th>
2026-01-20 22:58:40 +0700

    fix: use Elysia built-in functions

--- server/api/link.ts
+++ server/api/link.ts
@@ -1,36 +1,37 @@
-import {Elysia} from "elysia";
-import {pendingLinks} from "@discord/commands/preferences/link.ts";
+import { Elysia } from "elysia";
+import { pendingLinks } from "@discord/commands/preferences/link.ts";
 
 export function registerLinkAPI(app: Elysia) {
-  app.get("/api/link/:code", ({params}) => {
-    const {code} = params as unknown as { code: string };
+  app.get("/api/link/:code", ({ params, status }) => {
+    const { code } = params as unknown as { code: string };
 
     if (!code) {
-      return {status: 400};
+      return status(400);
     }
     const entry = [...pendingLinks.entries()].find(
-      ([, data]) => data.code === code && Date.now() - data.createdAt < 5 * 60 * 1000,
+      ([, data]) =>
+        data.code === code && Date.now() - data.createdAt < 5 * 60 * 1000,
     );
-    if (!entry) return {status: 404};
+    if (!entry) return status(404);
 
     const [_, data] = entry;
-    return {status: 200, data};
+    return status(200, data);
   });
 
-  app.get("/api/link/:code/delete", ({params}) => {
-    const {code} = params as unknown as { code: string };
+  app.get("/api/link/:code/delete", ({ params, status }) => {
+    const { code } = params as unknown as { code: string };
 
     if (!code) {
-      return {status: 400};
+      return status(400);
     }
 
     const entry = [...pendingLinks.entries()].find(
       ([, data]) => data.code === code,
     );
-    if (!entry) return {status: 404};
+    if (!entry) return status(404);
 
     const [discordID] = entry;
     pendingLinks.delete(discordID);
-    return {status: 200};
+    return { status: 200 };
   });
 }
