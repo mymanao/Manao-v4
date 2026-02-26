6fd928841d69ea4c910e191878046cc520146df0
Tinnaphat Somsang <tinvv@outlook.co.th>
2026-02-26 17:28:37 +0700

    fix(discord): cannot disable Discord

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import "@/client/twitch";
 import { run } from "@/client/discord";
 import { startServer } from "./server";
-import { KICK } from "@/config.ts";
+import {DISCORD, KICK} from "@/config.ts";
 import { startKickBot } from "@/client/kick";
 
-await run();
+if (DISCORD.ENABLED) {
+  await run();
+}
+
 startServer();
 
 if (KICK.ENABLED) {
